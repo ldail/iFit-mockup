908caf5c5478e5ec836b7d6549166d1b37e8d4be
ldail <leondailofficial@gmail.com>
2020-05-30 14:41:50 -0700

    static version finished and data compiled in info.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,149 @@
 import React from 'react';
-import logo from './logo.svg';
+import './reset.css';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+      <header className="header">
+        <div className="top-header">
+          <h1>iFIt Coach Icon</h1>
+          <button>Sign up</button>
+        </div>
+        <div className="info-header">
+          <ul>
+            <li>EXERCISE</li>
+            <li>NUTRITION</li>
+            <li>ACTIVITY</li>
+            <li>SLEEP</li>
+          </ul>
+        </div>
+      </header>
+      <main className="main">
+        <div className="hero-image">
+          <h2>
+            <span>The best personal training,</span>
+            <span>right in your own home</span>
+          </h2>
+          <button>Join iFit Coach</button>
+        </div>
+        <div className="reviews">
+          <ul>
+            <li>
+              <h4>CompanyName</h4>
+              <p>Review goes here.</p>
+            </li>
+            <li>
+              <h4>CompanyName</h4>
+              <p>Review goes here.</p>
+            </li>
+            <li>
+              <h4>CompanyName</h4>
+              <p>Review goes here.</p>
+            </li>
+          </ul>
+        </div>
+        <div className="videos">
+          <ul>
+            <li>
+              <img src="#" alt="video thumbnail" />
+              <div className="video-details">
+                <div className="video-title-info">
+                  <h5>Video title here</h5>
+                  <img src="#" alt="user avatar" />
+                </div>
+                <ul className="video-stats">
+                  <li>30 mins</li>
+                  <li>6m views</li>
+                </ul>
+                <a href="#" className="video-expand-link">VIEW DETAILS</a>
+              </div>
+            </li>
+            <li>
+              <img src="#" alt="video thumbnail" />
+              <div className="video-details">
+                <div className="video-title-info">
+                  <h5>Video title here</h5>
+                  <img src="#" alt="user avatar" />
+                </div>
+              </div>
+            </li>
+            <li>
+              <img src="#" alt="video thumbnail" />
+              <div className="video-details">
+                <div className="video-title-info">
+                  <h5>Video title here</h5>
+                  <img src="#" alt="user avatar" />
+                </div>
+                <ul className="video-stats">
+                  <li>30 mins</li>
+                  <li>6m views</li>
+                </ul>
+                <a href="#" className="video-expand-link">VIEW DETAILS</a>
+              </div>
+            </li>
+            <li>
+              <img src="#" alt="video thumbnail" />
+              <div className="video-details">
+                <div className="video-title-info">
+                  <h5>Video title here</h5>
+                  <img src="#" alt="user avatar" />
+                </div>
+              </div>
+            </li>
+          </ul>
+        </div>
+        <div className="equipment">
+          <h3>Interested in our exciting iFit-enabled equipment?</h3>
+          <ul>
+            <li className="equipment-item">
+              <img src="#" alt="exercise equipment" />
+              <span className="equipment-title">Treadmills</span>
+            </li>
+          </ul>
+        </div>
+      </main>
+      <footer className="footer">
+        <section className="footer-section">
+          <h6 className="footer-title">Company</h6>
+          <ul className="footer-list">
+            <li>About</li>
+            <li>Contact Us</li>
+            <li>Careers</li>
+          </ul>
+          <h6 className="footer-title">Account</h6>
+          <ul className="footer-list">
+            <li>Log In</li>
+            <li>Create Account</li>
+          </ul>
+          <h6 className="footer-title">Support</h6>
+          <ul className="footer-list">
+            <li>Help Center</li>
+            <li>Accessibility</li>
+          </ul>
+        </section>
+        <section className="footer-section">
+          <ul>
+            <li>`icon`</li>
+            <li>`icon`</li>
+            <li>`icon`</li>
+            <li>`icon`</li>
+            <li>`icon`</li>
+          </ul>
+        </section>
+        <section className="footer-section">
+          <select name="language-selection" id="language-selection">
+            <option value="English">English</option>
+          </select>
+        </section>
+        <section className="footer-section">
+          <ul>
+            <li>c iFit.com. All right reserved.</li>
+            <li>Privacy Policy</li>
+            <li>Terms of Use</li>
+          </ul>
+        </section>
+      </footer>
     </div>
   );
 }
---
+++ src/assets/info.js
@@ -0,0 +1,203 @@
+import {ReactComponent as WiredLogo} from '../assets/logos/wired-logo.svg';
+import {ReactComponent as MashableLogo} from '../assets/logos/mashable-logo.svg';
+import {ReactComponent as GearJunkieLogo} from '../assets/logos/gear-junkie-logo.svg';
+
+export const NAV_LIST = [
+  {
+    title: 'BLOG',
+    url: '#'
+  },
+  {
+    title: 'NOURISH',
+    url: '#'
+  },
+  {
+    title: 'SHOP',
+    url: '#'
+  }
+];
+
+export const SUBNAV_LIST = [
+  {
+    title: 'EXERCISE',
+    url: '#'
+  },
+  {
+    title: 'NUTRITION',
+    url: '#'
+  },
+  {
+    title: 'ACTIVITY',
+    url: '#'
+  },
+  {
+    title: 'SLEEP',
+    url: '#'
+  }
+];
+
+export const REVIEWS = [
+  {
+    companyName: 'GearJunkie',
+    companyLogo: GearJunkieLogo,
+    reviewInfo: 'You focus on putting in the hard work, and the technology handles the rest.'
+  },
+  {
+    companyName: 'WIRED',
+    companyLogo: WiredLogo,
+    reviewInfo: 'Literally transports you from home to wherever you choose to run.'
+  },
+  {
+    companyName: 'Mashable',
+    companyLogo: MashableLogo,
+    reviewInfo: 'Breathes new life into a tired, old running routine.'
+  }
+];
+
+export const VIDEOS = [
+  {
+    videoTitle: 'Lake Inniscarra, Ireland—Pyramid',
+    videoId: 1,
+    isSeries: false,
+    length: '30:53',
+    views: '6,248 M'
+  },
+  {
+    videoTitle: 'Performance Series',
+    vieoId: 2,
+    isSeries: true,
+    playlistCount: 9
+  },
+  {
+    videoTitle: 'Slow Pulls and Fast Intervals',
+    videoId: 3,
+    isSeries: false,
+    length: '44:13',
+    views: '9,948 M'
+  },
+  {
+    videoTitle: '20 Minutes to Toned',
+    vieoId: 4,
+    isSeries: true,
+    playlistCount: 12
+  },
+  {
+    videoTitle: 'Charles Race, Boston, Massachusetts',
+    videoId: 5,
+    isSeries: false,
+    length: '36:22',
+    views: '8,648 M'
+  },
+  {
+    videoTitle: 'Full-Body HIIT Series',
+    vieoId: 6,
+    isSeries: true,
+    playlistCount: 12
+  },
+  {
+    videoTitle: 'Kafue River, Zambia—Power Stroke Pyramid',
+    videoId: 7,
+    isSeries: false,
+    length: '22:22',
+    views: '4,660 M'
+  },
+  {
+    videoTitle: 'Shred & Burn Series',
+    vieoId: 6,
+    isSeries: true,
+    playlistCount: 16
+  },
+];
+
+export const EQUIPMENT = [
+  {
+    title: 'Treadmills',
+    image: '../assets/equipment/treadmills.png'
+  },
+  {
+    title: 'Bikes',
+    image: '../assets/equipment/bikes.png'
+  },
+  {
+    title: 'Ellipticals',
+    image: '../assets/equipment/ellipticals.png'
+  },
+  {
+    title: 'Strength',
+    image: '../assets/equipment/strength.png'
+  },
+];
+
+export const FOOTER_MAP = [
+  {
+    title: 'Company',
+    list: [
+      {
+        title: 'About',
+        url: '#'
+      },
+      {
+        title: 'Contact Us',
+        url: '#'
+      },
+      {
+        title: 'Careers',
+        url: '#'
+      }
+    ]
+  },
+  {
+    title: 'Account',
+    list: [
+      {
+        title: 'Log In',
+        url: '#'
+      },
+      {
+        title: 'Create Account',
+        url: '#'
+      }
+    ]
+  },
+  {
+    title: 'Support',
+    list: [
+      {
+        title: 'Help Center',
+        url: '#'
+      },
+      {
+        title: 'Accessibility',
+        url: '#'
+      }
+    ]
+  }
+];
+
+export const FOOTER_SOCIAL_LINKS = [
+  {
+    website: 'YouTube',
+    url: '#',
+    icon: '../assets/icons/youtube.png'
+  },
+  {
+    website: 'Pinterest',
+    url: '#',
+    icon: '../assets/icons/pinterest.png'
+  },
+  {
+    website: 'Facebook',
+    url: '#',
+    icon: '../assets/icons/facebook.png'
+  },
+  {
+    website: 'Twitter',
+    url: '#',
+    icon: '../assets/icons/twitter.png'
+  },
+  {
+    website: 'Instagram',
+    url: '#',
+    icon: '../assets/icons/instagram.png'
+  }
+];
