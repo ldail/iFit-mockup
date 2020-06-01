33fd8a90d31c87e5f6c79e581a9f582bf51e01a2
ldail <leondailofficial@gmail.com>
2020-05-31 23:08:05 -0700

    Updates

---
+++ precache-manifest.3bf4e19cf8811aaa5b8fc6a175b751e9.js
@@ -0,0 +1,150 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a1e18aa8178ea2edbdab709e7922d46a",
+    "url": "/iFit-mockup/index.html"
+  },
+  {
+    "revision": "b39090e7ad3555c01390",
+    "url": "/iFit-mockup/static/css/main.42aad4ea.chunk.css"
+  },
+  {
+    "revision": "162f1fbe691b9d8da419",
+    "url": "/iFit-mockup/static/js/2.9a927a02.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/iFit-mockup/static/js/2.9a927a02.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b39090e7ad3555c01390",
+    "url": "/iFit-mockup/static/js/main.33f4f625.chunk.js"
+  },
+  {
+    "revision": "bac2eb15cb7d8cd959e8",
+    "url": "/iFit-mockup/static/js/runtime-main.22d34e39.js"
+  },
+  {
+    "revision": "0870033b631176ac2bf27d830196a6fa",
+    "url": "/iFit-mockup/static/media/LeagueGothic-Regular.0870033b.svg"
+  },
+  {
+    "revision": "0ded65b4d18738a0cdab9067a371807c",
+    "url": "/iFit-mockup/static/media/LeagueGothic-Regular.0ded65b4.ttf"
+  },
+  {
+    "revision": "16eb3ff01d2cf227b8c517b9a251fd0d",
+    "url": "/iFit-mockup/static/media/LeagueGothic-Regular.16eb3ff0.woff"
+  },
+  {
+    "revision": "2642ce2d95694e46fd6c3cd79e56a846",
+    "url": "/iFit-mockup/static/media/LeagueGothic-Regular.2642ce2d.eot"
+  },
+  {
+    "revision": "cad4b3591ecdfe89ecf86ff8ebcba6e8",
+    "url": "/iFit-mockup/static/media/LeagueGothic-Regular.cad4b359.otf"
+  },
+  {
+    "revision": "2955a25ab3c95d6aadc0bd5626f9161a",
+    "url": "/iFit-mockup/static/media/ProximaNova-Light.2955a25a.eot"
+  },
+  {
+    "revision": "33b405831a3a3175b0a8f18ae8930cf3",
+    "url": "/iFit-mockup/static/media/ProximaNova-Light.33b40583.ttf"
+  },
+  {
+    "revision": "a96f368fe56a476335ef73b7c8164810",
+    "url": "/iFit-mockup/static/media/ProximaNova-Light.a96f368f.woff"
+  },
+  {
+    "revision": "ac51ad8a36d5bab6da11d454bcd06561",
+    "url": "/iFit-mockup/static/media/ProximaNova-Light.ac51ad8a.otf"
+  },
+  {
+    "revision": "c495a0577228edf99d679125b1d078af",
+    "url": "/iFit-mockup/static/media/ProximaNova-Light.c495a057.svg"
+  },
+  {
+    "revision": "192efbe1ca8d2b4b6153075d31dcf2b5",
+    "url": "/iFit-mockup/static/media/ProximaNova-Regular.192efbe1.svg"
+  },
+  {
+    "revision": "5caa8a3ec55403605b2b0499be59c31d",
+    "url": "/iFit-mockup/static/media/ProximaNova-Regular.5caa8a3e.woff"
+  },
+  {
+    "revision": "6d40e4d465208b4ab030f3d8ce8628d2",
+    "url": "/iFit-mockup/static/media/ProximaNova-Regular.6d40e4d4.eot"
+  },
+  {
+    "revision": "feface72008e6857da27f939e1eceffa",
+    "url": "/iFit-mockup/static/media/ProximaNova-Regular.feface72.ttf"
+  },
+  {
+    "revision": "b17c657fa31bc4c96297218ab739165a",
+    "url": "/iFit-mockup/static/media/bikes.b17c657f.png"
+  },
+  {
+    "revision": "29ff69e87c7b485a303ac07bf56c8266",
+    "url": "/iFit-mockup/static/media/ellipticals.29ff69e8.png"
+  },
+  {
+    "revision": "7af5d0f6aafca3b2d2f7277e35ca0a1b",
+    "url": "/iFit-mockup/static/media/gear-junkie-logo.7af5d0f6.svg"
+  },
+  {
+    "revision": "26fece2ba8acefa3ce8feca7e22cbea7",
+    "url": "/iFit-mockup/static/media/hero.26fece2b.png"
+  },
+  {
+    "revision": "67aa7f4fbc8db6a7cc7cb553486f954f",
+    "url": "/iFit-mockup/static/media/ifit-coach-logo.67aa7f4f.svg"
+  },
+  {
+    "revision": "ebea61d04c909b9434ed46d0b9995986",
+    "url": "/iFit-mockup/static/media/mashable-logo.ebea61d0.svg"
+  },
+  {
+    "revision": "87b89c727d67b7cdeca679622d3ba6c9",
+    "url": "/iFit-mockup/static/media/strength.87b89c72.png"
+  },
+  {
+    "revision": "be777edcba6e9b53b0c62040c8208b99",
+    "url": "/iFit-mockup/static/media/treadmills.be777edc.png"
+  },
+  {
+    "revision": "d9317bb23b403ae04200f7189322f18a",
+    "url": "/iFit-mockup/static/media/video1thumbnail.d9317bb2.png"
+  },
+  {
+    "revision": "c97f1f00c1cfc81f605877cbc4932f59",
+    "url": "/iFit-mockup/static/media/video2thumbnail.c97f1f00.png"
+  },
+  {
+    "revision": "2c5d11acc74826c2c369c40850d3f772",
+    "url": "/iFit-mockup/static/media/video3thumbnail.2c5d11ac.png"
+  },
+  {
+    "revision": "65432c7d1f5a2a4392fa0af14e2d1b3d",
+    "url": "/iFit-mockup/static/media/video4thumbnail.65432c7d.png"
+  },
+  {
+    "revision": "874edca1e850806a484964babd42bcee",
+    "url": "/iFit-mockup/static/media/video5thumbnail.874edca1.png"
+  },
+  {
+    "revision": "3a895e9ad35b764ccdc0f3cce97b2048",
+    "url": "/iFit-mockup/static/media/video6thumbnail.3a895e9a.png"
+  },
+  {
+    "revision": "dec7125f0618a86beb3c74f9df1c15ea",
+    "url": "/iFit-mockup/static/media/video7thumbnail.dec7125f.png"
+  },
+  {
+    "revision": "8b04ec5af9b4576df7203c5f740f20de",
+    "url": "/iFit-mockup/static/media/video8thumbnail.8b04ec5a.png"
+  },
+  {
+    "revision": "993909fd8c212d3dab834f373ed79ce2",
+    "url": "/iFit-mockup/static/media/wired-logo.993909fd.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/ifit/precache-manifest.47143df4ec9b4326dfba8a2870cbd2c7.js"
+  "/iFit-mockup/precache-manifest.3bf4e19cf8811aaa5b8fc6a175b751e9.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/ifit/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/iFit-mockup/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
